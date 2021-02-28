6ec6aa6908e74236cb85cc48ad0449fa7ba0cf07
ecalzo <evancalz@me.com>
2021-02-28 11:25:21 -0500

    feat: allow for multiple button presses

--- src/producer.py
+++ src/producer.py
@@ -34,11 +34,21 @@ def clean_chat_input(chat_text: str) -> str:
 def push_to_queue_if_valid_input(message: str, queue: Queue) -> str:
     if message.startswith("!"):
         inpt_as_list = message.lower().split("!")
-        len_check = len(inpt_as_list)
-        if len_check > 1 and len_check < 3:
-            button_press = inpt_as_list[-1].strip()
+        chat_command = inpt_as_list[-1].split()
+        len_check = len(chat_command)
+        if len_check == 1:
+            button_press = chat_command[0].strip()
             queue.put(button_press)
             return button_press
+        elif len_check > 1 and len_check < 3:
+            button_press = chat_command[0].strip()
+            num_presses = chat_command[1].strip()
+            try:
+                limit_presses = min(int(num_presses), 10)
+            except ValueError:
+                limit_presses = 1
+            for _ in range(limit_presses):
+                queue.put(button_press)
     return ""
 
 
